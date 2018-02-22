65e1490fee7cc72b24f2bbe017c33812d34caae2
D <dimcoyk@gmail.com>
2018-02-23 02:36:35 +0300

    Modify validation algorithm

--- TestTomcatIncProj/src/main/java/by/Team/CRUD/Validate/Validator.java
+++ TestTomcatIncProj/src/main/java/by/Team/CRUD/Validate/Validator.java
@@ -7,14 +7,11 @@ import by.Team.CRUD.DAO.UserDAOImpl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Validator {
     private final String SELECT_Request = "select * from users";
     private PreparedStatement preparedStatement = null;
     private UserDAOImpl userDAO;
-    private List<Integer> ListID = new ArrayList<Integer>();
     public Validator(UserDAO userDAO){
         this.userDAO = (UserDAOImpl)userDAO;
     }
@@ -25,17 +22,10 @@ public class Validator {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()){
-                ListID.add(resultSet.getInt(1));
-            }
-
-            for (int i = 0; i < ListID.size(); i++){
-                if (ListID.get(i) == user.getId()){
+                if(user.getId() == resultSet.getInt(1)){
                     return true;
                 }
-
-                System.out.println(ListID.get(i));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
