376785493fd0118931265360061163c9e5f48505
D <dimcoyk@gmail.com>
2018-03-13 10:09:06 +0300

    Add jump between pages, change controller structure, fix some bags

--- SpringWebApp/src/main/java/com/Dimcooo/controllers/HomeController.java
+++ SpringWebApp/src/main/java/com/Dimcooo/controllers/HomeController.java
@@ -9,17 +9,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/authorization")
-public class AuthorizationController {
+public class HomeController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/signUp")
-    public @ResponseBody String signUpUser(@ModelAttribute("userJSP") UsersEntity user){
-//        return userService.RegisterUser(user);
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String startConfig(){
+        return "start_page";
+    }
+
+    @RequestMapping(value = "/signUp", method = RequestMethod.GET)
+    public String jumpToPageGet(Model model){
+        model.addAttribute("userJSP", new UsersEntity());
+        return "signUp_page";
+    }
+
+    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
+    public @ResponseBody
+    String signUpUser(@ModelAttribute("userJSP") UsersEntity user){
         return "Everybody be cool";
     }
 }
--- SpringWebApp/src/main/webapp/WEB-INF/pages/start_page.jsp
+++ SpringWebApp/src/main/webapp/WEB-INF/pages/start_page.jsp
@@ -1,15 +1,13 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+
 <html>
     <head>
         <title>Title</title>
     </head>
     <body>
-    <%--@elvariable id="userJSP" type="com.Dimcooo"--%>
-    <spring:form method="post" modelAttribute="userJSP" action="/authorization/signUp">
-        Name: <spring:input path="name"/>
-        Age: <spring:input path="age"/>
-        <spring:button>Sign Up</spring:button>
-    </spring:form>
+        <a href="${contextPath}/signUp">Press</a>
     </body>
 </html>
